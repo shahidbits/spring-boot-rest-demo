15bc0d3febd7a9529251b1f311a9f3962ad0c0a8
Mohammad Shahid <mohammad.shahid@walmartlabs.com>
2019-09-26 01:34:52 +0530

    Added multiple ways of versioning

---
+++ src/main/java/com/example/springbootin28min/restfulwebservices/versioning/Name.java
@@ -0,0 +1,30 @@
+package com.example.springbootin28min.restfulwebservices.versioning;
+
+public class Name {
+    private String fName;
+    private String lName;
+
+    public Name() {
+    }
+
+    public Name(String fName, String lName) {
+        this.fName = fName;
+        this.lName = lName;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+}
